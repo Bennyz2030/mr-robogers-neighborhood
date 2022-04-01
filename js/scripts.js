1df2ba5fa9e67f8a97ba9c12aaaa65c617b364d7
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-01 09:31:27 -0700

    WIP second test

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ function roboTranslate(number) {
   let outputArray = [];
   for (i=0; i<=number; i++) {
     outputArray.push([i]);
+    if ([i].includes("1")) {
+      outputArray.slice(i, 1, "Beep!");
+    }
   }
   return outputArray;
 }
