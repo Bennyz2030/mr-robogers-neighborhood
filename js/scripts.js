7b723e6b44eb8c11420885c2eb541150015b81c8
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-01 09:56:05 -0700

    pass second test

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-function roboTranslate(number) {
-  let outputArray = [];
-  for (i=0; i<=number; i++) {
-    outputArray.push([i]);
-    if ([i].includes("1")) {
-      outputArray.slice(i, 1, "Beep!");
+function roboTranslate(input) {
+  const outputArray = [];
+  for (i=0; i<=input; i++) {
+  outputArray.push([i].toString()); {
+  if ([i].toString().includes("1")) {
+    outputArray.splice(i, 1, "Beep!");
+      }
     }
   }
   return outputArray;
